588c23545f3ec9991bed7706f49077ef4c7f79d0
jeffuofa93 <wiederkehr@email.arizona.edu>
2022-03-07 12:30:24 -0700

    update

--- src/create.js
+++ src/create.js
@@ -1,17 +1,13 @@
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import handler from "./util/handler";
+import dynamoDb from "./util/dynamodb";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export async function main(event) {
-  // Request body is passed in as a JSON encoded string in 'event.body'
+export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
-
   const params = {
     TableName: process.env.TABLE_NAME,
     Item: {
       // The attributes of the item to be created
-      // userID hardcoded for now going to be set with auth later
       userId: "123", // The id of the author
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
@@ -20,17 +16,7 @@ export async function main(event) {
     },
   };
 
-  try {
-    await dynamoDb.put(params).promise();
+  await dynamoDb.put(params);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-  } catch (e) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message }),
-    };
-  }
-}
+  return params.Item;
+});
